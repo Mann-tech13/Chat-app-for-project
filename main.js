7bf2b9398d96d0a7b16d1f7fef0cc790d507908a
Mann-tech13 <mannshah.ict19@gmail.com>
2021-07-25 16:10:43 +0530

    Chat-app plane UI

--- main.js
+++ main.js
@@ -7,11 +7,35 @@ const mann = document.querySelector("#mann");
 
 function chatStarted() {
     const chatList = document.querySelector("#chatList");
+    const message_workspace = document.querySelector(".message-workspace");
+
     if (chatList.classList.contains('clicked') == false) {
         chatList.className += "clicked";
     }
+
+}
+// const click = document.querySelectorAll(".click");
+function myfunctNandan() {
+    chatStarted();
+    const me = document.querySelector(".profile").innerHTML = "Nandan";
 }
 
-function myfunct() {
+function myfunctAniket() {
     chatStarted();
+    const me = document.querySelector(".profile").innerHTML = "Aniket"
+}
+
+function myfunctMohit() {
+    chatStarted();
+    const me = document.querySelector(".profile").innerHTML = "Mohit"
+}
+
+function myfunctAryan() {
+    chatStarted();
+    const me = document.querySelector(".profile").innerHTML = "Aryan"
+}
+
+function myfunctMann() {
+    chatStarted();
+    const me = document.querySelector(".profile").innerHTML = "Mann"
 }
