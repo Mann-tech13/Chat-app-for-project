f2cfd411792f6883d625b4935e1f523eb9248b46
Mann-tech13 <mannshah.ict19@gmail.com>
2021-07-29 20:13:24 +0530

    Alternate UI of chat app

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,44 +1,31 @@
+"use strict";
+
 // $( '.friend-drawer--onhover' ).on( 'click',  function() {
-
 //   $( '.chat-bubble' ).hide('slow').show('slow');
-
 // });
-
-
-
-
-
 // function chatStarted() {
 //   const chatList = document.querySelector("#chatList");
 //   const message_workspace = document.querySelector(".message-workspace");
-
 //   if (chatList.classList.contains('clicked') == false) {
 //       chatList.className += "clicked";
 //   }
-
 // }
-
 function myfunctNandan() {
-
-    const me = document.querySelector("#profile").innerHTML = "Nandan Shukla";
+  var me = document.querySelector("#profile").innerHTML = "Nandan Shukla";
 }
 
 function myfunctAniket() {
-
-    const me = document.querySelector("#profile").innerHTML = "Aniket Nadkarni"
+  var me = document.querySelector("#profile").innerHTML = "Aniket Nadkarni";
 }
 
 function myfunctMohit() {
-
-    const me = document.querySelector("#profile").innerHTML = "Mohit Shah"
+  var me = document.querySelector("#profile").innerHTML = "Mohit Shah";
 }
 
 function myfunctAryan() {
-
-    const me = document.querySelector("#profile").innerHTML = "Aryan Sheth"
+  var me = document.querySelector("#profile").innerHTML = "Aryan Sheth";
 }
 
 function myfunctMann() {
-
-    const me = document.querySelector("#profile").innerHTML = "Mann Shah"
+  var me = document.querySelector("#profile").innerHTML = "Mann Shah";
 }
